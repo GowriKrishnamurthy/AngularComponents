19a9a2d5a6e755b803dd172eb9fd57827d67b3e6
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-05-28 14:34:01 +1000

    CRUD operations using HTTP Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { SignupFormComponent } from './signup-form/signup-form.component';
 import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
 import { ChangePasswordFormComponent } from './change-password-form/change-password-form.component';
 import { PostsComponent } from './posts/posts.component'; 
+import { PostService } from './services/post.service';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,7 @@ import { PostsComponent } from './posts/posts.component';
     BrowserModule,
     HttpModule
   ],
-  providers: [AuthorsService],
+  providers: [AuthorsService,PostService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,19 +1,50 @@
-import { Component } from '@angular/core';
-import { Http } from '@angular/http';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../services/post.service';
 
 @Component({
   selector: 'posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent  {
-
-  posts:any[];
-  constructor(http:Http) { 
-    http.get('http://jsonplaceholder.typicode.com/posts')
+export class PostsComponent implements OnInit {
+  ngOnInit(): void {
+    this.service.getPosts()
     .subscribe(response=>{
       this.posts = response.json();
     });
   }
+  posts:any[];
   
+  constructor(private service:PostService) { 
+  }
+  
+  createPost(input:HTMLInputElement){
+    let post ={title:input.value};
+
+    this.service.createPost(post)
+      .subscribe(response=>{
+        // Get the id from the server
+        post['id']=response.json().id;
+        //Show it on the UI-- add this new item to the array to the beginning.
+        this.posts.splice(0,0,post);
+        //Clear input field
+        input.value='';
+        console.log(response.json());
+    });
+  }
+
+  updatePost(post){
+      this.service.updatePost(post)
+      .subscribe(response=>{
+      console.log(response.json());
+    })
+  }
+
+  deletePost(post){
+    this.service.deletePost(post.id)
+    .subscribe(response=>{
+      let index=this.posts.indexOf(post);
+      this.posts.splice(index,1);
+    });
+  }
 }
