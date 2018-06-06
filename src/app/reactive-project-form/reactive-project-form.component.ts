3f31400db02c6288e8e67eb660e4749ac6fe391e
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-06-06 19:25:29 +1000

    Rective project Form- Custom and Async validators added

--- src/app/reactive-project-form/reactive-project-form.component.ts
+++ src/app/reactive-project-form/reactive-project-form.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CustomValidators } from './custom-validators';
 
 @Component({
-  selector: 'app-reactive-project-form',
+  selector: 'reactive-project-form',
   templateUrl: './reactive-project-form.component.html',
   styleUrls: ['./reactive-project-form.component.css']
 })
 export class ReactiveProjectFormComponent implements OnInit {
-
-  constructor() { }
+  projectForm : FormGroup;
+  // projectStatus:['Stable', 'Critical', 'Finished'];
 
   ngOnInit() {
+    this.projectForm=new FormGroup({
+      'projectName':new FormControl(null,  
+        [Validators.required,
+        CustomValidators.invalidProjectName.bind(this)],
+        CustomValidators.asyncInvalidProjectName),
+      'email':new FormControl(null,[Validators.required,Validators.email]),
+      'projectStatus':new FormControl('Stable')
+      });
+  }
+  
+  get projectName(){
+    return this.projectForm.get('projectName');
+  }
+  get email(){
+    return this.projectForm.get('email');
+  }
+  get projectStatus(){
+    return this.projectForm.get('projectStatus');
   }
 
+  onSubmit()
+  {
+    console.log(this.projectForm.value);
+  }
 }
