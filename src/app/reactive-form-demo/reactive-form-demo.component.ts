22d7bb34282a4a0b905be6718082d374ee4fffa7
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-06-06 12:59:27 +1000

    Angular Reactive Form: Sync up Html form and Typescript form

--- src/app/reactive-form-demo/reactive-form-demo.component.ts
+++ src/app/reactive-form-demo/reactive-form-demo.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'reactive-form',
   templateUrl: './reactive-form-demo.component.html',
   styleUrls: ['./reactive-form-demo.component.css']
 })
-export class ReactiveFormDemoComponent {
-  genders = ['male', 'female'];
+export class ReactiveFormDemoComponent implements OnInit {
 
+  genders = ['male', 'female'];
+  myReactiveForm: FormGroup;
+
+  ngOnInit(): void {
+    this.myReactiveForm=new FormGroup({
+      'username':new FormControl(null),
+      'email':new FormControl(null),
+      'gender':new FormControl('female'),
+    });
+  }
 
 }
