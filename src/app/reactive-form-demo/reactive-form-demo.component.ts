2bc1c2d3d507a48bb22840c757c6e9700d1f8bfa
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-06-06 17:50:37 +1000

    Reactive Form: Form Array, Custom validators, Prepopulating the form

--- src/app/reactive-form-demo/reactive-form-demo.component.ts
+++ src/app/reactive-form-demo/reactive-form-demo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'reactive-form',
@@ -9,15 +10,37 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class ReactiveFormDemoComponent implements OnInit {
   genders = ['male', 'female'];
   myReactiveForm: FormGroup;
-  
+  forbiddenUsernames = ['Smith', 'John'];
+
   ngOnInit() {
     this.myReactiveForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
-        'email': new FormControl(null, [Validators.required, Validators.email])
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails)
       }),
       'gender': new FormControl('female'),
+      'hobbies': new FormArray([])
     });
+    // this.myReactiveForm.valueChanges.subscribe(
+    //   (value) => console.log(value)
+    // );
+    this.myReactiveForm.statusChanges.subscribe(
+      (status) => console.log(status)
+    );
+    this.myReactiveForm.setValue({
+      'userData': {
+        'username': 'Gk',
+        'email': 'Gk@test.com'
+      },
+      'gender': 'female',
+      'hobbies': []
+    });
+    this.myReactiveForm.patchValue({
+      'userData': {
+        'username': 'Gow',
+      }
+    });
+
   }
 
   get username() {
@@ -28,6 +51,29 @@ export class ReactiveFormDemoComponent implements OnInit {
     return this.myReactiveForm.get('userData.email');
   }
   onSubmit() {
-
+    console.log(this.myReactiveForm)
+    this.myReactiveForm.reset();
+  }
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.myReactiveForm.get('hobbies')).push(control);
+  }
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUsernames.indexOf(control.value) != -1) {
+      return { 'nameIsForbidden': true };
+    }
+    return null;
+  }
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({ 'emailIsForbidden': true });
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
   }
 }
