cbb5c10ad09fa9966703b6ed8e7cea191840a3ef
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-06-06 17:53:51 +1000

    React form assignment - New component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { PostService } from './services/post.service';
 import { NewUserFormComponent } from './new-user-form/new-user-form.component';
 import { FormAssignmentComponent } from './form-assignment/form-assignment.component';
 import { ReactiveFormDemoComponent } from './reactive-form-demo/reactive-form-demo.component';
+import { ReactFormAssignmentComponent } from './react-form-assignment/react-form-assignment.component';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,8 @@ import { ReactiveFormDemoComponent } from './reactive-form-demo/reactive-form-de
     PostsComponent,
     NewUserFormComponent,
     FormAssignmentComponent,
-    ReactiveFormDemoComponent],
+    ReactiveFormDemoComponent,
+    ReactFormAssignmentComponent],
   imports: [
     FormsModule,
     ReactiveFormsModule,
---
+++ src/app/react-form-assignment/react-form-assignment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'react-form-assignment',
+  templateUrl: './react-form-assignment.component.html',
+  styleUrls: ['./react-form-assignment.component.css']
+})
+export class ReactFormAssignmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
