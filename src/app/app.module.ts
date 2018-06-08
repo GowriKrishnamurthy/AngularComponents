1fbe5cba2b33734b3f02fc99ca6d5e8d4d356a8f
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-06-08 10:38:58 +1000

    ngClass binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpModule, Http } from '@angular/http';
 import { AppComponent } from './app.component';
 import { LikeComponent } from './like/like.component';
-import { AuthorsComponent } from './authors/authors.component'; 
+import { AuthorsComponent } from './authors/authors.component';
 import { AuthorsService } from './authors.service';
 import { CustomTitleCasePipe } from './custom-title-case.pipe';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -14,12 +14,14 @@ import { ZippyComponent } from './zippy/zippy.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
 import { ChangePasswordFormComponent } from './change-password-form/change-password-form.component';
-import { PostsComponent } from './posts/posts.component'; 
+import { PostsComponent } from './posts/posts.component';
 import { PostService } from './services/post.service';
 import { NewUserFormComponent } from './new-user-form/new-user-form.component';
 import { FormAssignmentComponent } from './form-assignment/form-assignment.component';
 import { ReactiveFormDemoComponent } from './reactive-form-demo/reactive-form-demo.component';
 import { ReactiveProjectFormComponent } from './reactive-project-form/reactive-project-form.component';
+import { NgClassBindingComponent } from './ng-class-sample/ng-class-sample.component';
+
 
 @NgModule({
   declarations: [
@@ -38,14 +40,15 @@ import { ReactiveProjectFormComponent } from './reactive-project-form/reactive-p
     NewUserFormComponent,
     FormAssignmentComponent,
     ReactiveFormDemoComponent,
-    ReactiveProjectFormComponent],
+    ReactiveProjectFormComponent,
+    NgClassBindingComponent],
   imports: [
     FormsModule,
     ReactiveFormsModule,
     BrowserModule,
     HttpModule
   ],
-  providers: [AuthorsService,PostService],
+  providers: [AuthorsService, PostService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/ng-class-sample/ng-class-sample.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-class-binding',
+  templateUrl: './ng-class-sample.component.html',
+  styleUrls: ['./ng-class-sample.component.css']
+})
+export class NgClassBindingComponent{
+  servers = [
+    {
+      instanceType: 'medium',
+      name: 'Production Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'large',
+      name: 'User Database',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Development Server',
+      status: 'offline',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Testing Environment Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    }
+  ];
+  getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
+    return {
+      'list-group-item-success': server.status === 'stable',
+      'list-group-item-warning': server.status === 'offline',
+      'list-group-item-danger': server.status === 'critical'
+    };
+  }
+}
